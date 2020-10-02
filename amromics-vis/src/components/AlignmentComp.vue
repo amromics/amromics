12bb857cd8597e064d629ebf3a81abdfe5eb1430
Lê Đức Quang <leducquangpm@gmail.com>
2020-10-02 16:01:02 +0700

    add gene clusters table, first interconnected mechanism

--- amromics-vis/src/components/AlignmentComp.vue
+++ amromics-vis/src/components/AlignmentComp.vue
@@ -8,6 +8,7 @@
 <script>
 /* eslint-disable */
 import {AlignmentViewer} from "@/amromicsjs";
+import EventBus from '@/event-bus.js';
 // import SampleIGV from "@/components/Visualization/IGV";
 export default {
     name: 'AlignmentComp',
@@ -15,6 +16,7 @@ export default {
     data() {
         return {
             loading: false,
+            list_alignments:[]
           
         };
     },
@@ -23,6 +25,7 @@ export default {
       var ctx=document.getElementById('aligmentview');
       //console.log(this.core_data);
       //console.log(Phylogeny);
+      var list_alignments=this.alignmentData.alignments;
       var alignmentview = new AlignmentViewer(ctx);
       
       var tree_data=atob(this.alignmentData.alignments[0].tree).replace(/.ref/g,"");  
@@ -31,13 +34,24 @@ export default {
       alignmentview.load(this.alignmentData.alignments[0].gene,tree_data,this.alignmentData.alignments[0].samples);
       
       alignmentview.draw();
+      EventBus.$on('gene_id_emited', gene_id => {
+        console.log('gene_id_emited'+gene_id);
+        for(var i=0;i<list_alignments.length;i++){
+          if (list_alignments[i].gene==gene_id){
+            alignmentview.load(this.alignmentData.alignments[i].gene,tree_data,this.alignmentData.alignments[i].samples);      
+            alignmentview.draw();
+            break;
+          }
+        }
+        
+      });
       this.loading = false;
     },
     async created() {
 
       
     },
-    method: {
+    methods: {
       
     }
 };
