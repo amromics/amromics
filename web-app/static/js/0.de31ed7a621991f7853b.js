159d565f6859c5610babbc12c026c87a2f1f2d0b
Lê Đức Quang <leducquangpm@gmail.com>
2020-11-10 17:15:58 +0700

    add web-app folder for quick run

---
+++ web-app/static/js/0.de31ed7a621991f7853b.js
@@ -0,0 +1,9 @@
+webpackJsonp([0],{WbLe:function(n,t,o){var e,a,r;
+/*! DataTables styling integration
+ * ©2018 SpryMedia Ltd - datatables.net/license
+ */
+/*! DataTables styling integration
+ * ©2018 SpryMedia Ltd - datatables.net/license
+ */
+r=function(n,t,o,e){return n.fn.dataTable},e=[o("7t+N"),o("FNwM")],void 0===(a=function(n){return r(n,window,document)}.apply(t,e))||(n.exports=a)}});
+//# sourceMappingURL=0.de31ed7a621991f7853b.js.map
